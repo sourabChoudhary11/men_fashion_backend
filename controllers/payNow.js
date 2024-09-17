a4aab30b3d90f55f0224278c6056107b5ff2ec9c
sourabChoudhary11 <123931473+sourabChoudhary11@users.noreply.github.com>
2024-09-18 00:23:30 +0530

    Update payNow.js

--- controllers/payNow.js
+++ controllers/payNow.js
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const PayNow = async (req, res, next) => {
-    const { amount, customerName, customerAddress } = req.body;
+    const { amount, customerName, customerAddress, customerCity, customerState, customerCountry, customerPostalCode } = req.body;
   
     try {
       const paymentIntent = await stripe.paymentIntents.create({
@@ -11,8 +11,12 @@ const PayNow = async (req, res, next) => {
         currency: "inr",
         description: "Export of men's casual and formal wear, including shirts, jeans, shoes, perfume and watch",
         metadata: {
-        customerName,
+         customerName,
         customerAddress,
+        customerCity,
+        customerState,
+        customerCountry,
+        customerPostalCode,
           }
       });
   
