be14fe94a6b0328195ed16cb0b6e7bf10b278d99
sourabChoudhary11 <123931473+sourabChoudhary11@users.noreply.github.com>
2024-09-20 00:17:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import productRoute from "./routes/product.js";
 import orderRoute from "./routes/order.js";
 import {GetCookie} from "./middlewares/auth.js";
 import cartItemRoute from "./routes/cartItem.js";
-import PayNow from "./controllers/payNow.js";
+import paymentRoute from "./controllers/payment.js";
 
 dotenv.config();
 
@@ -38,11 +38,11 @@ app.get("/", (req, res) => {
 
 // specify the routes
 app.get("/get-cookie", GetCookie);
-app.post("/pay-now", PayNow);
 app.use("/user", userRoute);
 app.use("/product", productRoute);
 app.use("/order", orderRoute);
 app.use("/cart", cartItemRoute);
+app.use("/payment", paymentRoute);
 
 app.get("*", (req, res) => {
   res.status(404).send(`404 - Page Not Found`);
